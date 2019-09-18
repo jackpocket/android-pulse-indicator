1a44759e5fb3268620dec95190e1e06f79be0a6d
Matthew Silber <matt@jackpocket.com>
2019-09-18 10:44:59 -0400

    Move src into pulse-indicator, created pulse-indicator-sample

---
+++ pulse-indicator-sample/src/main/java/com/jackpocket/pulse/test/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.jackpocket.pulse.test
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import com.jackpocket.pulse.layouts.PulsingLinearLayout
+import com.jackpocket.pulse.test.R
+
+class MainActivity: AppCompatActivity() {
+
+    private val pulseLayout: PulsingLinearLayout
+        get() = findViewById(R.id.main__pulse_layout)
+
+    private val pulseTarget: AppCompatImageView
+        get() = findViewById(R.id.main__pulse_target_image)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onTestPulseClicked(view: View?) {
+        Log.d(TAG, "Pulse starting...")
+
+        pulseLayout.pulseController
+                .setCirclePathOverride(true)
+                .setPulsingColor(0x22FF22)
+                .setPulsingStrokeWidth(10)
+                .setDurationMs(1500)
+                .setPulseLifeSpanMs(900)
+                .setRespawnRateMs(300)
+                .setAlphaInterpolator(AccelerateInterpolator())
+                .setScaleInterpolator(LinearInterpolator())
+                .setFinishedListener({  _ ->
+                    Log.d(TAG, "Pulse completed.")
+                })
+                .attachTo(this, pulseTarget)
+    }
+
+    companion object {
+
+        const val TAG = "PulseIndicatorTest"
+    }
+}
